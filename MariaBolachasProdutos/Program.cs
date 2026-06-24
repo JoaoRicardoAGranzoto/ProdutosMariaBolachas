0638e84755f3d58817a61b24320e230ec38e2a78
Joao Ricardo Araujo Granzoto <joaoricardo.granzoto@gmail.com>
2026-06-23 23:39:41 -0300

    Primeira rota de API, só um array simples que retorna as bolachas

--- MariaBolachasProdutos/Program.cs
+++ MariaBolachasProdutos/Program.cs
@@ -10,3 +10,14 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapGet("/", () =>  new[] { 
+    new { Id = 1, Nome = "Sq Leite Condensado", Preco = 43.50m },
+    new { Id = 2, Nome = "Semi Folhado com Goiabada", Preco = 43.50m },
+    new { Id = 3, Nome = "Bolacha de Morango", Preco = 45.00m }
+});
+
+
+
+
+app.Run();
