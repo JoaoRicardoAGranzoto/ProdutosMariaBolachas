adcceb8a9ebf31f08a1408948b18e547de67d0d4
Joao Ricardo Araujo Granzoto <joaoricardo.granzoto@gmail.com>
2026-06-23 23:47:51 -0300

    Swagger adicionado

--- MariaBolachasProdutos/Program.cs
+++ MariaBolachasProdutos/Program.cs
@@ -11,13 +11,21 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("/", () =>  new[] { 
+//Primeira rota da API, que retorna uma lista de bolachas com seus respectivos preços
+app.MapGet("/Bolachas", () =>  new[] { 
     new { Id = 1, Nome = "Sq Leite Condensado", Preco = 43.50m },
     new { Id = 2, Nome = "Semi Folhado com Goiabada", Preco = 43.50m },
-    new { Id = 3, Nome = "Bolacha de Morango", Preco = 45.00m }
+    new { Id = 3, Nome = "Beliscao", Preco = 43.50m }
+
+
+
+
 });
 
-
-
+app.MapGet("/Roscas", () =>  new[] { 
+    new { Id = 1, Nome = "Rosca de Creme P", Preco = 43.50m },
+    new { Id = 2, Nome = "Rosca de Chocolate", Preco = 43.50m },
+    new { Id = 3, Nome = "Rosca de Chocolate", Preco = 43.50m }
+});
 
 app.Run();
