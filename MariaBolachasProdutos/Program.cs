6140b7771cbc7d2d17f27e95cbe5ef444a3c540f
Joao Ricardo Araujo Granzoto <joaoricardo.granzoto@gmail.com>
2026-06-25 15:16:01 -0300

    Adicionando post para inserir um novo dado

--- MariaBolachasProdutos/Program.cs
+++ MariaBolachasProdutos/Program.cs
@@ -15,34 +15,42 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-var bolachas = new List<Bolachas>()
+var alimentos = new List<Alimentos>()
 {
-    new Bolachas { Id = 1, Nome = "Sequilho Leite Condensado", Preco = 43.50m, Estoque = 100 },
-    new Bolachas { Id = 2, Nome = "Sequilho Creme de Leite E Limao", Preco = 43.50m, Estoque = 150 },
-    new Bolachas { Id = 3, Nome = "Nata", Preco = 43.50m, Estoque = 80 },
-    new Bolachas { Id = 4, Nome = "Sequilho Coco", Preco = 43.50m, Estoque = 230 },
-    new Bolachas { Id = 5, Nome = "Sequilho Maracuja com Chocolate", Preco = 43.50m, Estoque = 90 },
-    new Bolachas { Id = 6, Nome = "Sequilho Nata e Coco", Preco = 43.50m, Estoque = 123 },
-    new Bolachas { Id = 7, Nome = "Nata com Goiabada", Preco = 43.50m, Estoque = 230 },
+    new Alimentos { Id = 1, Nome = "Sequilho Leite Condensado", Preco = 43.50m, Estoque = 100 },
+    new Alimentos { Id = 2, Nome = "Sequilho Creme de Leite E Limao", Preco = 43.50m, Estoque = 150 },
+    new Alimentos { Id = 3, Nome = "Nata", Preco = 43.50m, Estoque = 80 },
+    new Alimentos { Id = 4, Nome = "Sequilho Coco", Preco = 43.50m, Estoque = 230 },
+    new Alimentos { Id = 5, Nome = "Sequilho Maracuja com Chocolate", Preco = 43.50m, Estoque = 90 },
+    new Alimentos { Id = 6, Nome = "Sequilho Nata e Coco", Preco = 43.50m, Estoque = 123 },
+    new Alimentos { Id = 7, Nome = "Nata com Goiabada", Preco = 43.50m, Estoque = 230 },
+    new Alimentos { Id = 8, Nome = "Rosca Chocolate G", Preco = 54.50m, Estoque = 2 },
+    new Alimentos { Id = 9, Nome = "Rosca Chocolate P", Preco = 38.23m, Estoque = 2 }
 };
 
 app.UseHttpsRedirection();
 
-app.MapGet("/bolachas", () =>
+app.MapGet("/alimentos", () =>
 {
-    return bolachas;
+    return alimentos;
 });
 
-app.MapGet("/bolachas/{id}", (int id) =>
+app.MapGet("/alimentos/{id}", (int id) =>
 {
-    var bolacha = bolachas.FirstOrDefault(b => b.Id == id);
+    var alimento = alimentos.FirstOrDefault(a => a.Id == id);
     
-    return bolacha is not null ? Results.Ok(bolacha) : Results.NotFound($"Bolacha com ID {id} não encontrada.");
+    return alimento is not null ? Results.Ok(alimento) : Results.NotFound($"Alimento com ID {id} não encontrado.");
+});
+
+app.MapPost("/alimentos", (Alimentos novoAlimento) =>
+{
+    alimentos.Add(novoAlimento);
+    return Results.Created();
 });
 
 app.Run();
 
-class Bolachas
+class Alimentos
 {
     public int Id { get; set; }
     public required string Nome { get; set; }
